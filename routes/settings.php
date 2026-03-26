0ac01377b65974821489a147b10dc41b5ec10559
Sabri Taieb <sabritaiebdev@gmail.com>
2026-03-26 16:12:38 +0100

    refactor: remove orphaned route

--- routes/settings.php
+++ routes/settings.php
@@ -16,8 +16,6 @@ Route::middleware(['auth'])->group(function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('settings/security', [SecurityController::class, 'edit'])->name('security.edit');
-
     Route::put('settings/password', [SecurityController::class, 'update'])
         ->middleware('throttle:6,1')
         ->name('user-password.update');
