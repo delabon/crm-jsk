322b47f8a3eca3105bab0cc0bd502800f39ba627
Sabri Taieb <sabritaiebdev@gmail.com>
2026-03-28 08:35:11 +0100

    refactor: add redirect back to \App\Http\Controllers\Auth\EmailVerificationController::__invoke

--- app/Http/Controllers/Auth/EmailVerificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationController.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 final class EmailVerificationController extends Controller
 {
-    public function __invoke(Request $request): void
+    public function __invoke(Request $request): RedirectResponse
     {
         $request->user()->sendEmailVerificationNotification();
 
-        session()->flash('success', 'Verification link sent!');
+        return back()
+            ->with('success', 'Verification link sent!');
     }
 }
