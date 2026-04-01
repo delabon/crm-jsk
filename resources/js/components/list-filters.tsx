e262b72f4e794c6d16aa7a93c3b4ff5d06c82794
Sabri Taieb <sabritaiebdev@gmail.com>
2026-04-01 12:31:55 +0100

    refactor: improve the list-filters.tsx component

--- resources/js/components/list-filters.tsx
+++ resources/js/components/list-filters.tsx
@@ -1,7 +1,7 @@
 import {Form, Link} from "@inertiajs/react";
 import {ChevronDownIcon, ChevronUpIcon, XIcon} from "lucide-react";
 import type {PropsWithChildren} from "react";
-import {useState} from "react";
+import {useState, useRef} from "react";
 import {Button} from "@/components/ui/button";
 import {cn} from "@/lib/utils";
 
@@ -10,6 +10,7 @@ type Props = PropsWithChildren<{
 }>;
 
 export default function ListFilters({children, action}: Props) {
+    const dropdownRef = useRef<HTMLDivElement>(null);
     const [isVisible, setIsVisible] = useState(false);
     const toggleFilters = () => {
         setIsVisible(!isVisible);
@@ -24,17 +25,17 @@ export default function ListFilters({children, action}: Props) {
             <ChevronUpIcon className={isVisible ? 'inline-flex' : 'hidden'}/>
         </Button>
         <Form action={action} method="GET">
-            <div className={cn("w-full md:w-88 fixed inset-0 md:absolute md:top-[110%] md:right-0 md:left-auto md:bottom-auto overflow-hidden md:rounded-lg md:border md:border-gray-300 md:shadow-lg", isVisible ? 'flex' : 'hidden')}>
+            <div ref={dropdownRef} className={cn("w-full md:w-88 fixed inset-0 md:absolute md:top-[110%] md:right-0 md:left-auto md:bottom-auto overflow-hidden md:rounded-lg md:border md:border-gray-300 md:shadow-lg", isVisible ? 'flex' : 'hidden')}>
                 <div className="text-gray-800 w-full flex flex-col gap-4 bg-white  p-4  md:max-h-96 overflow-y-auto">
                     <div className="flex items-center justify-between gap-3">
                         <span className="font-semibold">Filters</span>
                         <Button
-                            className="md:hidden"
+                            className="!px-0"
                             onClick={toggleFilters}
-                            variant="outline"
+                            variant="link"
                             type="button"
                         >
-                            <XIcon size={16} className="text-foreground"/>
+                            <XIcon size={16} className="text-black"/>
                         </Button>
                     </div>
                     {children}
