d002380e1b3f58392fe65d7a2d6b749ac03608ac
Sabri Taieb <sabritaiebdev@gmail.com>
2026-04-02 10:26:07 +0100

    refactor: improve filter.tsx component

--- resources/js/components/filter.tsx
+++ resources/js/components/filter.tsx
@@ -14,11 +14,11 @@ export default function Filter({children, title}: Props) {
     };
 
     return <div className="dashboard-list-filter flex flex-col gap-3 border-b border-gray-200 pb-3">
-        <div onClick={toggleFilter} className="cursor-pointer hover:opacity-80 text-sm font-semibold flex items-center justify-between gap-3">
+        <button type="button" onClick={toggleFilter} className="cursor-pointer hover:opacity-80 text-sm font-semibold flex items-center justify-between gap-3">
             <span>{title}</span>
             <ChevronDownIcon size={16} className={isVisible ? 'hidden' : 'inline-flex'}/>
             <ChevronUpIcon size={16} className={isVisible ? 'inline-flex' : 'hidden'}/>
-        </div>
+        </button>
         <div className={cn("text-sm", isVisible ? 'block' : 'hidden')}>{children}</div>
     </div>
 }
