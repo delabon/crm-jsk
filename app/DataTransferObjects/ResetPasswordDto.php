8a747cec7ce71113fafbb0369169c77a27dbabcc
Sabri Taieb <sabritaiebdev@gmail.com>
2026-04-06 05:53:29 +0100

    refactor: introduce ResetPasswordDto and update accordingly

---
+++ app/DataTransferObjects/ResetPasswordDto.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataTransferObjects;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+final readonly class ResetPasswordDto implements Arrayable
+{
+    public function __construct(
+        public string $email,
+        public string $token,
+        public string $password,
+    ) {}
+
+    public function toArray(): array
+    {
+        return [
+            'email' => $this->email,
+            'token' => $this->token,
+            'password' => $this->password,
+            'password_confirmation' => $this->password,
+        ];
+    }
+}
